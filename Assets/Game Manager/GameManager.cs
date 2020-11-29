27cd4038d033ea0848ed67391ec5e44899c87a08
Herman Lederer <germans.lederers@gmail.com>
2020-11-29 16:02:55 +0100

    Add speech bubble functionality and reorganize assets

---
+++ Assets/Game Manager/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Odyssey
+{
+	public class GameManager : MonoBehaviour
+	{
+		// Editor fields
+		[SerializeField] private GameObject speechBubble;
+
+		public static GameManager Instance { get; private set; }
+
+		#region MonoBehaviour methods
+
+		private void Awake()
+		{
+			if (Instance != null)
+			{
+				Destroy(this);
+				return;
+			}
+
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+
+		private void Start()
+		{
+
+		}
+
+		private void Update()
+		{
+
+		}
+
+		#endregion
+
+		#region GameManager methods
+
+		public void MakeCoice1()
+		{
+			PresentChoiceConsequences();
+		}
+
+		public void MakeCoice2()
+		{
+			PresentChoiceConsequences();
+		}
+
+		public void PresentChoice()
+		{
+			SpeechBubble.Instance.Show();
+		}
+
+		public void PresentChoiceConsequences()
+		{
+			StartCoroutine(ShowChoiceConsequences());
+		}
+
+		private IEnumerator ShowChoiceConsequences()
+		{
+			SpeechBubble.Instance.Hide();
+			yield return new WaitForSeconds(1f);
+			SpeechBubble.Instance.Show();
+		}
+
+		#endregion
+	}
+}
+
---
+++ Assets/Speech Bubble/SpeechBubble.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Odyssey
+{
+	public class SpeechBubble : MonoBehaviour
+	{
+		public static SpeechBubble Instance { get; private set; }
+
+		#region MonoBehaviour methods
+
+		private void Awake()
+		{
+			if (Instance != null)
+			{
+				Destroy(this);
+				return;
+			}
+
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+
+		private void Start()
+		{
+
+		}
+
+		private void Update()
+		{
+
+		}
+
+		#endregion
+
+		#region SpeechBubble methods
+
+		public void Show()
+		{
+			gameObject.SetActive(true);
+		}
+
+		public void Hide()
+		{
+			gameObject.SetActive(false);
+		}
+
+		#endregion
+	}
+}
