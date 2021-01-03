2aed874ff1ece157f33085a79ab0f2d2f9556886
Herman Lederer <germans.lederers@gmail.com>
2021-01-03 20:51:53 +0100

    Implement first cutscene

---
+++ Assets/Factory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+	// Editor components
+	[SerializeField] private Sprite spriteOpen;
+	[SerializeField] private Sprite spriteShutDown;
+	[SerializeField] private ParticleSystem smokeParticles;
+
+	public void Start()
+	{
+		Open();
+	}
+
+	public void Open()
+	{
+		GetComponent<SpriteRenderer>().sprite = spriteOpen;
+		smokeParticles.Play();
+	}
+
+	public void ShutDown()
+	{
+		GetComponent<SpriteRenderer>().sprite = spriteShutDown;
+		smokeParticles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+	}
+}
